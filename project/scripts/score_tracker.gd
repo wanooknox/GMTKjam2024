a33ee0530f9b85ecc47971fd7b47461ac9959ac8
kirypto <kirypto@gmail.com>
2024-08-17 12:47:53 -0600

    Update score_tracker and ui_score_updater to use 2 lines between methods

--- project/scripts/score_tracker.gd
+++ project/scripts/score_tracker.gd
@@ -3,13 +3,16 @@ extends Node
 signal score_changed
 var _current_score: int = 0
 
+
 func increase_score(amount: int) -> void:
 	_current_score += amount
 	emit_signal("score_changed")
 
+
 func reset_score() -> void:
 	_current_score = 0
 	emit_signal("score_changed")
+
 
 func get_score() -> int:
 	return _current_score
--- project/scripts/ui_score_updater.gd
+++ project/scripts/ui_score_updater.gd
@@ -2,9 +2,11 @@ extends Node
 
 @onready var score_label: Label = $ScoreText # Shorthand: get 'ScoreText' child from node this script is attached to
 
+
 func _ready() -> void:
 	ScoreTracker.connect("score_changed", _on_score_changed) # Link up to the score_changed event on ScoreTracker
 	_on_score_changed()
+
 
 func _on_score_changed() -> void:
 	score_label.text = str(ScoreTracker.get_score())
