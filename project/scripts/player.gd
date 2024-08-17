4785a5ef8beee0d4151ae4dc3076a90495ec2692
wanooknox <wanooknox@gmail.com>
2024-08-17 13:02:14 -0600

    setup actions for keybindings and implement platform fall through

--- project/scripts/player.gd
+++ project/scripts/player.gd
@@ -1,25 +1,34 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -300.0
-
+@export var SPEED:float = 150.0
+@export var JUMP_VELOCITY:float = -300.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	var on_floor:bool = is_on_floor()
+	var just_jumped:bool = Input.is_action_just_pressed("jump")
+
+	if Input.is_action_pressed("look_down") and just_jumped and on_floor:
+		# handle drop down
+		drop()
+	elif just_jumped and on_floor:
+		# Handle jump.
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func drop():
+	position.y += 2
