1f07b70d86c25d95ce10511a211fe8ee875cdd1a
defau1t <robin5693@gmail.com>
2024-08-17 11:31:58 -0600

    Spacebar now switches scene as well

--- project/scripts/welcome_screen.gd
+++ project/scripts/welcome_screen.gd
@@ -5,11 +5,10 @@ extends Control
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_SPACE:
+			_on_button_pressed()
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://rooms/game.tscn")
