4e9f6c7adb8d316ae506cc77ad4704de7c8e0896
kirypto <kirypto@gmail.com>
2024-08-18 14:31:28 -0600

    Modify interaction with bold to require being held for a certain amount of time

--- project/scripts/interactable-bolt.gd
+++ project/scripts/interactable-bolt.gd
@@ -3,23 +3,32 @@ extends Node2D
 @export var total_point_value: int = 150
 @export var single_hit_point_val: int = 10
 @export var last_hit_point_val: int = 50
+@export var seconds_per_animation_step: float = 0.5
 
 @onready var sprite:AnimatedSprite2D = $AnimatedSprite2D
 @onready var last_frame: int = sprite.sprite_frames.get_frame_count("default") - 1
 
 var _intersecting_player: Node2D
+var _bolt_progress_time: float = 0
 
 
 func _ready() -> void:
 	ScoreTracker.increase_score(-1 * total_point_value)
 
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if _intersecting_player != null:
-		if Input.is_action_just_released("interact") and _intersecting_player.get_current_tool_type() == "drill":
-			advance_sequence()
+		if Input.is_action_pressed("interact") and _intersecting_player.get_current_tool_type() == "drill":
+			advance_sequence(delta)
 
-func advance_sequence() -> void:
+func advance_sequence(additional_progress: float) -> void:
+	_bolt_progress_time += additional_progress
+
+	if _bolt_progress_time < seconds_per_animation_step:
+		# Need to keep holding to make progress
+		return
+
+	_bolt_progress_time -= seconds_per_animation_step
 	if sprite.frame != last_frame:
 		if sprite.frame == last_frame - 1:
 			ScoreTracker.increase_score(last_hit_point_val)
