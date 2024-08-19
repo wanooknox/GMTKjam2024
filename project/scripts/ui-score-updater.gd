7d5b19d7860fc0d004bd344e393eeec1340d6861
kirypto <kirypto@gmail.com>
2024-08-18 21:24:05 -0600

    Add a count down timer

--- project/scripts/ui-score-updater.gd
+++ project/scripts/ui-score-updater.gd
@@ -1,11 +1,36 @@
 extends Node
 
-@onready var score_label: Label = $TextBackground/ScoreText # Shorthand: get 'ScoreText' child from node this script is attached to
+@export var eta_hard_s: float = 60
+@export var eta_medium_s: float = 150
+@export var eta_easy_s: float = 450
+
+@onready var score_label: Label = $TextBackground/ScoreText
+@onready var eta_label: Label = $TextBackground/EtaText
+
+var _eta_s: float = eta_easy_s
 
 
 func _ready() -> void:
-	ScoreTracker.score_changed.connect(_on_score_changed) # Link up to the score_changed event on ScoreTracker
+	var difficulty: String = "easy" # TODO wire this up to some global config
+	match difficulty:
+		"easy":
+			_eta_s = eta_easy_s
+		"medium":
+			_eta_s = eta_medium_s
+		"hard":
+			_eta_s = eta_hard_s
+		_:
+			print("Error: Unknown difficulty " + difficulty + ", using easy")
+			_eta_s = eta_easy_s
+	ScoreTracker.score_changed.connect(_on_score_changed)
 	_on_score_changed()
+
+
+func _process(delta: float) -> void:
+	_eta_s -= delta
+	eta_label.text = "ETA: " + str(int(_eta_s)) + " seconds"
+	if _eta_s < 0:
+		eta_label.text = "Game Over" # TODO load the game end scene
 
 
 func _on_score_changed() -> void:
