c85d26d148e64c0881d9a53d1f2993e549912f0f
wanooknox <wanooknox@gmail.com>
2024-08-19 23:54:05 -0600

    reset score between runs

--- project/scripts/welcome_screen.gd
+++ project/scripts/welcome_screen.gd
@@ -2,7 +2,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	ScoreTracker.reset_score()
 
 
 func _process(_delta: float) -> void:
