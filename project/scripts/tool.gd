13d952b36b0e6a4eeab148f2c06a14aa8caa62dd
wanooknox <wanooknox@gmail.com>
2024-08-20 00:09:53 -0600

    make hit sfx less constant

--- project/scripts/tool.gd
+++ project/scripts/tool.gd
@@ -1,14 +1,23 @@
 extends Node
 
 @export var type: String = "changeme"
+@export var hit_sfx_delay_cutoff: float = 0.1
 
 
 var _player: Node2D = null
+var _hit_sfx_delay: float = 0.0
+var _hit_sfx_allowed: bool = true
 
 
 func _process(_delta: float) -> void:
 	if _player != null and Input.is_action_just_pressed("interact"):
 		_player.pickup_tool.emit(type, self)
+
+	if _hit_sfx_delay > hit_sfx_delay_cutoff:
+		_hit_sfx_allowed = true
+		_hit_sfx_delay = 0.0
+	else:
+		_hit_sfx_delay += _delta
 
 
 func _pickup_area_entered(body: Node2D) -> void:
@@ -22,10 +31,8 @@ func _on_pickup_area_exited(_body: Node2D) -> void:
 	_player = null
 
 
-func _on_body_entered(body:Node) -> void:
-	AudioPlayer.play("fx", "hit")
+func _on_body_entered(_body:Node) -> void:
+	if _hit_sfx_allowed:
+		AudioPlayer.play("fx", "hit")
+		_hit_sfx_allowed = false
 
-
-
-func _on_body_exited(body:Node) -> void:
-	AudioPlayer.play("fx", "hit")
